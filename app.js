5e699ea5a164a7f787c320121d2f322370e8bd6a
alageshbhavi <alagesan06@gmail.com>
2026-02-01 20:51:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 function calc1() {
   const n = parseFloat(document.getElementById("n1").value);
   const p = parseFloat(document.getElementById("p1").value);
-  const r = (p / 100) * n;
-  document.getElementById("r1").textContent = r;
+  document.getElementById("r1").textContent = ((p / 100) * n);
 }
 
 function calc2() {
   const x = parseFloat(document.getElementById("x2").value);
   const y = parseFloat(document.getElementById("y2").value);
-  const r = (x / y) * 100;
-  document.getElementById("r2").textContent = r + "%";
+  document.getElementById("r2").textContent = ((x / y) * 100) + "%";
 }
 
 function calc3() {
   const o = parseFloat(document.getElementById("old3").value);
   const n = parseFloat(document.getElementById("new3").value);
-  const r = ((n - o) / o) * 100;
-  document.getElementById("r3").textContent = r + "%";
+  document.getElementById("r3").textContent = (((n - o) / o) * 100) + "%";
 }
 
 function calc4() {
   const o = parseFloat(document.getElementById("old4").value);
   const n = parseFloat(document.getElementById("new4").value);
-  const r = ((o - n) / o) * 100;
-  document.getElementById("r4").textContent = r + "%";
+  document.getElementById("r4").textContent = (((o - n) / o) * 100) + "%";
 }
